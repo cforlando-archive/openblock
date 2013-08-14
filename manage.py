53dbc8803e15e117c99841d5948d5520b869b640
achen <achen@cfa.(none)>
2013-08-13 20:09:50 -0400

    Added dj-database-url to requirements.txt

--- manage.py
+++ manage.py
@@ -2,6 +2,17 @@
 import os
 import sys
 
+from django.core.management import execute_manager
+try:
+    import settings # Assumed to be in the same directory.
+except ImportError:
+    import sys
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.exit(1)
+
+if __name__ == "__main__":
+    execute_manager(settings)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openblock.settings")
 
